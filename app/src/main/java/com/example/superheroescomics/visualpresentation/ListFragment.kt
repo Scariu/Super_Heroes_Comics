c57669a770c56b8e5677ff24992022a78ce198cb
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-31 20:31:16 -0400

    Se inicializa adapter en ListFragment y se agrega permiso de internet a manifest.

--- app/src/main/java/com/example/superheroescomics/visualpresentation/ListFragment.kt
+++ app/src/main/java/com/example/superheroescomics/visualpresentation/ListFragment.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.superheroescomics.R
+import androidx.fragment.app.activityViewModels
 import com.example.superheroescomics.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     lateinit var binding: FragmentListBinding
+    private val viewModel: SuperHeroViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentListBinding.inflate(layoutInflater)
-
+        initAdapter()
         return binding.root
+    }
+
+    private fun initAdapter() {
+        viewModel.getSuperHeroesViewModel()
+        val adapter = ListAdapter()
+        binding.recyclerViewList.adapter = adapter
+        viewModel.superHeroesLiveData().observe(viewLifecycleOwner) {
+            adapter.setData(it)
+        }
     }
 }
